027b88490768cf67a715ca40d10245a7b32407f7
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-04-12 02:21:47 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton("AbOuT", callback_data = "about"),
+                    InlineKeyboardButton('AnIMe', url='https://t.me/Anime_Nation')
                 ]
             ]
         )
