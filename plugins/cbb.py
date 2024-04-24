e16cdd1dc2aa675c283cc00c301f504d6f096d49
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-04-25 01:54:06 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>TEEGRIXX</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/grixxtech'>GRIXXTECH</a>\n○ 18+ : <a href='https://t.me/upcomicc'>MANHWA</a>\n○ MANHWA : <a href='https://t.me/Manhwa_clubb'>MANHWA CLUB</a>\n○ ANIME CLUBB : <a hrefhttps://t.me/Anime_clubb_ac '>LATEST ANIMES</a>/n○ MANHWA CLOUD : <a href='https://t.me/manhwa_Clouud'>CLOUD ☁️</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>TEEGRIXX</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/grixxtech'>GRIXXTECH</a>\n○ 18+ : <a href='https://t.me/upcomicc'>MANHWA</a>\n○ MANHWA : <a href='https://t.me/Manhwa_clubb'>MANHWA CLUB</a>\n○ ANIME CLUBB : <a hrefhttps://t.me/Anime_clubb_ac '>LATEST ANIMES</a>\n○ MANHWA CLOUD : <a href='https://t.me/manhwa_Clouud'>CLOUD ☁️</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
