560ca9d0ce6642ffdc8422fae7a1dbf4c5aece53
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2024-04-25 00:52:25 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>TEEGRIXX</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/grixxtech'>GRIXXTECH</a>\n○ ANIME : <a href='https://t.me/Anime_Nation'>ANIME NATION</a>\n○ MANGA : <a href='https://t.me/Manga_Tron'>MANGATRON</a>\n○ ANIME ONGOING : <a href='https://t.me/AnimeNation_Ongoing'>ONGOING ANIMES</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>TEEGRIXX</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/grixxtech'>GRIXXTECH</a>\n○ MANHWA : <a href='https://t.me/Anime_Nation'>ANIME NATION</a>\n○ MANGA : <a href='https://t.me/Manga_Tron'>MANGATRON</a>\n○ ANIME ONGOING : <a href='https://t.me/AnimeNation_Ongoing'>ONGOING ANIMES</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
